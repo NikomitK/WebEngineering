3574d3474b0cf46b9161bfda2cc0f3686b251d1c
Niko <nikobo2003@gmail.com>
2023-05-24 14:26:04 +0200

    simplify win condition

--- Vorlesung3/Aufgaben/tictactoe/script.js
+++ Vorlesung3/Aufgaben/tictactoe/script.js
@@ -3,56 +3,42 @@ let currentPlayer = 'X';
 let gameOver = false;
 
 function makeMove(index) {
-    if(board[index] === '') {
+    if (board[index] === '') {
         document.getElementsByClassName("cell")[index].innerHTML = currentPlayer;
         board[index] = currentPlayer;
 
-        if(checkWin()){
+        if (checkWin()) {
             console.log(`Current player ${currentPlayer} has won!`)
             gameOver = true;
         }
-        if(gameOver || !board.includes('')) {
+        if (gameOver || !board.includes('')) {
             gameOver = true;
             console.log('ende gelände')
         }
 
-        currentPlayer = currentPlayer == 'X' ? 'O' : 'X' 
+        currentPlayer = currentPlayer == 'X' ? 'O' : 'X'
     }
 }
 
-function checkWin(){
+function checkWin() {
     //return if the tic tac toe game has been won
-    if(board[0] == board[1] && board[1] == board[2] && board[0] != ''){
-        return true;
-    }
-    if(board[3] == board[4] && board[4] == board[5] && board[3] != ''){
-        return true;
-    }
-    if(board[6] == board[7] && board[7] == board[8] && board[6] != ''){
-        return true;
-    }
-    if(board[0] == board[3] && board[3] == board[6] && board[0] != ''){
-        return true;
-    }
-    if(board[1] == board[4] && board[4] == board[7] && board[1] != ''){
-        return true;
-    }
-    if(board[2] == board[5] && board[5] == board[8] && board[2] != ''){
-        return true;
-    }
-    if(board[0] == board[4] && board[4] == board[8] && board[0] != ''){
-        return true;
-    }
-    if(board[2] == board[4] && board[4] == board[6] && board[2] != ''){
-        return true;
-    }
+    if ((board[0] === board[1] && board[1] === board[2] && board[0] !== '') ||
+        (board[3] === board[4] && board[4] === board[5] && board[3] !== '') ||
+        (board[6] === board[7] && board[7] === board[8] && board[6] !== '') ||
+        (board[0] === board[3] && board[3] === board[6] && board[0] !== '') ||
+        (board[1] === board[4] && board[4] === board[7] && board[1] !== '') ||
+        (board[2] === board[5] && board[5] === board[8] && board[2] !== '') ||
+        (board[0] === board[4] && board[4] === board[8] && board[0] !== '') ||
+        (board[2] === board[4] && board[4] === board[6] && board[2] !== '')
+    ) return true;
     return false;
+
 
 }
 
 function resetGame() {
     // to be implemente
-    for(let i = 0; i<9; i++){
+    for (let i = 0; i < 9; i++) {
         board[i] = ''
         document.getElementsByClassName("cell")[i].innerHTML = ''
     }
