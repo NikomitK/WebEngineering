dbaa2d1ac468a3bd6515e12c45cc886be2367e1a
Niko <nikobo2003@gmail.com>
2023-06-14 16:45:24 +0200

    vorlesung5

---
+++ Vorlesung5/github_client/script.js
@@ -0,0 +1,41 @@
+const accesstoken = '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';
+
+list = document.getElementById('repos-list')
+message = document.getElementById('message')
+
+async function getRepos() {
+
+    console.log('test')
+    token = document.getElementById("password").value
+
+    fetch('https://api.github.com/user/repos', {
+        headers: {
+            'Authorization': "Bearer " + token
+        }
+    })
+        .then(res => {
+
+            if (res.ok) {
+                message.innerHTML = "Hat geklappt :)";
+                return res;
+            }
+            else {
+                throw ("Skill issue");
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            data.forEach(element => {
+                listElement = document.createElement("li");
+                elementName = document.createTextNode(element.name);
+                listElement.appendChild(elementName);
+                list.appendChild(listElement);
+            })
+        })
+        .catch(err => message.innerHTML = "Fehler: " + err + " :(");
+}
+
+function showError(msg) {
+    message.innerHtml = msg;
+}
